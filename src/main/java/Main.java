941faa10e95c357126be3c97c3bd7c9af16794e6
aMiT <asj8139@g.rit.edu>
2017-12-02 14:35:45 -0500

    Created a singleton class of POSTagger such that standfordNLP object and Properties list object are created only once and used for all paragraphs. disabled logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,6 @@
  * @Revisions: $ Log: Initial Revision $
  */
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.CoreMap;
-
-import java.util.List;
-import java.util.Properties;
-
 /**
  * The {@code Main} class 
  *
@@ -23,20 +14,10 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) {
-        Properties props = new Properties();
-        props.setProperty("annotators","tokenize, ssplit, pos");
 
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation annotation = new Annotation("I'm so happy about my marks");
-        pipeline.annotate(annotation);
-        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-        for (CoreMap sentence : sentences) {
-            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-                System.out.println(word + "\t" + pos);
-            }
-        }
+        POSTagger posTagger = POSTagger.getInstance();
+        System.out.println(posTagger.getTaggedWords("I'm so happy about my marks"));
+
     }
 
 }
---
+++ src/main/java/POSTagger.java
@@ -0,0 +1,62 @@
+/**
+ * POSTagger.java
+ *
+ * @Version: $ Id: POSTagger.java, v 1.0 12/2/2017 2:15 PM $
+ * @Revisions: $ Log: Initial Revision $
+ */
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * The {@code POSTagger} class 
+ *
+ * @author : Amit Shyam Jaisinghani
+ * @author : Aditi Shailendra Singhai
+ */
+public class POSTagger {
+
+    private StanfordCoreNLP pipeline;
+    private static POSTagger posTagger;
+
+    private POSTagger() {
+        Properties props = new Properties();
+        props.setProperty("annotators","tokenize, ssplit, pos");
+
+        pipeline = new StanfordCoreNLP(props);
+    }
+
+    public List<Pair<String, String>> getTaggedWords(String text) {
+        List<Pair<String, String>> listOfTaggedWords = new ArrayList<>();
+        Annotation annotation = new Annotation(text);
+        pipeline.annotate(annotation);
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                listOfTaggedWords.add(new Pair<>(word, pos));
+            }
+        }
+        return listOfTaggedWords;
+    }
+
+    public static POSTagger getInstance() {
+
+        if(posTagger == null)
+            posTagger = new POSTagger();
+
+        return posTagger;
+    }
+
+
+}
